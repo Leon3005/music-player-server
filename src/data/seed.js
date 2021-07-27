b751b1f4a24efe542a0490f12f2765c29a649753
Leon <leonwheeler08@gmail.com>
2021-07-27 09:31:37 +0100

    Successfully seeded requests

--- src/data/seed.js
+++ src/data/seed.js
@@ -1,10 +1,15 @@
 const db = require("../config/connection");
 const { Request } = require("../models");
+const requests = require("./requests");
 
 db.once("open", async () => {
   try {
     // Delete all models
     await Request.deleteMany({});
+
+    // Seed requests
+    await Request.insertMany(requests);
+    console.log("Requests seeded successfully!!!");
 
     process.exit(0);
   } catch (error) {
