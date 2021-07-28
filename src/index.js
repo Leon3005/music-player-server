dc756f33cec6e4581563a4a0dd3b8274fd2b52f5
Leon <leonwheeler08@gmail.com>
2021-07-28 08:58:50 +0100

    Moved CORS

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
